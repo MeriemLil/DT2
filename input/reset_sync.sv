e43690ef91361b019ee99f96c37c2b8d413262f2
Meriem Aggoune <maggoune20@kataja2.oulu.fi>
2020-12-22 22:34:27 +0200

    reset_sync

--- input/reset_sync.sv
+++ input/reset_sync.sv
@@ -7,7 +7,26 @@ module reset_sync
    input logic rst_n,
    output logic srst_n   
    );
+   
+   logic q1_r;
+   logic q2_r;
+   
+   always_ff@(posedge clk or negedge rst_n)
+      begin
+	if (rst_n == '0)
+	  begin
+	    q1_r <= '0;
+ 	    q2_r <= '0;
+          end
 
+	else
+	  begin
+	    q1_r <= '1;
+	    q2_r <= q1_r;
+ 	  end
+      end
+
+   assign srst_n = q2_r;
    
 endmodule
 
