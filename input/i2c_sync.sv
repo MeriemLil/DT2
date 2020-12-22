f04315bd8144a31cdc3a9956378f581142ae246c
Meriem Aggoune <maggoune20@kataja2.oulu.fi>
2020-12-22 16:05:12 +0200

    i2c_sync

--- input/i2c_sync.sv
+++ input/i2c_sync.sv
@@ -17,6 +17,53 @@ module i2c_sync
    );
 
    
+   logic scl_sff1_r;
+   logic scl_sff2_r;
+   logic sda_sff1_r;
+   logic sda_sff2_r;
+   logic past_scl_r;
+   logic past_sda_r;
+
+   always_ff@(posedge clk or negedge rst_n)
+     begin: sync_ffs
+	if (rst_n == '0)
+	 begin
+	  scl_sff1_r <= '1;
+          scl_sff2_r <= '1;
+          sda_sff1_r <= '1;
+          sda_sff2_r <= '1;
+	 end
+	
+	else
+	 begin
+	  scl_sff1_r <= scl_in;
+	  sda_sff1_r <= sda_in;
+	  scl_sff2_r <= scl_sff1_r;
+	  sda_sff2_r <= sda_sff1_r;
+	 end
+     end
+
+
+   always_ff@(posedge clk or negedge rst_n)
+     begin: edgedet_ffs
+	if (rst_n == '0)
+	 begin
+	  past_sda_r <= '1;
+	  past_scl_r <= '1;
+	 end
+
+	else
+	 begin
+	  past_scl_r <= scl_sff2_r;
+	  past_sda_r <= sda_sff2_r;
+	 end
+     end 
+
+   assign scl_out = scl_sff2_r;
+   assign past_scl_out = past_scl_r;
+   assign past_sda_out = past_sda_r;
+   assign sda_out = sda_sff2_r;
+   
 endmodule
 
 
