fb4307bb5de354939fcbef52b14f3bd1080816cd
Meriem Aggoune <maggoune20@kataja2.oulu.fi>
2020-12-23 14:44:05 +0200

    i2c_ctr3

--- input/i2c_ctr.sv
+++ input/i2c_ctr.sv
@@ -12,7 +12,69 @@ module i2c_ctr
    output logic frameok_out   
    );
 
+   logic [3:0] bitctr_r;
+   logic [$clog2(I2C_FRAME_BYTES)-1:0] bytectr_r;
+   logic seven;
+
+   always_ff@(posedge clk or negedge rst_n)
+     begin
+	if (rst_n == '0)
+	   bitctr_r <= '0;
+        else 
+	  begin
+	     if (clr_in == '1)
+ 	   	bitctr_r <= '0;
+             else if (next_in == '1 && bitctr_r < 8)
+		bitctr_r <= bitctr_r + 1;
+	     else if (next_in == '1 && bitctr_r >= 8)
+ 	   	bitctr_r <= '0;
+	     else
+	        bitctr_r <= bitctr_r;
+          end
+     end
+
+   always_comb
+     begin
+        if (bitctr_r == 7)
+	   seven = '1;
+        else
+	   seven = '0;
+
+	if (bitctr_r == 8)
+	   byteok_out = '1;
+	else
+	   byteok_out = '0; 
+     end 
+
+
+   always_ff@(posedge clk or negedge rst_n)
+     begin
+     	if (rst_n == '0)
+           bytectr_r <= '0;
+        else 
+	   begin
+	     if (byteen_in == '0)
+		bytectr_r <= '0;
+             else 
+	      begin
+		if (next_in == '1 && seven == '1 && bytectr_r < I2C_DATA_BYTES && clr_in == '0)
+		   bytectr_r <= bytectr_r + 1;	
+             	else if (next_in == '1 && seven == '1 && bytectr_r >= I2C_DATA_BYTES && clr_in == '0)
+                   bytectr_r <= '0;
+	        else 
+		   bytectr_r <= bytectr_r;
+              end 
+	   end
+     end
+
+   always_comb
+     begin
+	if (bytectr_r == I2C_DATA_BYTES)
+	   frameok_out = '1;
+        else
+           frameok_out = '0;
+     end
+
    
 endmodule
-
 
