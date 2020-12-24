6263e32c89b89f92ae1079edded58fc83fbf9acf
Meriem Aggoune <maggoune20@kataja2.oulu.fi>
2020-12-24 18:51:48 +0200

    demem

--- input/dmem.sv
+++ input/dmem.sv
@@ -14,6 +14,45 @@ module dmem
    output logic [DATABITS-1:0] 	      d_out
    );
 
+  logic [DMEMSIZE-1:0][DATABITS-1:0] mem_r;
+  logic [DMEMSIZE*DATABITS-1:0] mem_tmp;
+
+   always_ff@(posedge clk or negedge rst_n)
+      begin:mem_reg
+	if (rst_n == '0)
+	   begin
+	     mem_r <= '0;
+	     mem_tmp = '0;
+	   end
+	else
+	   begin
+	     if (sde_in == '1)
+		begin
+		  mem_tmp = mem_r;
+		  mem_r <= {mem_tmp[$bits(mem_tmp)-2:0], sd_in};
+		end
+	     else if (cmd_in == DMEM_SHIFT)
+		begin
+		  for(int i = 1; i < DMEMSIZE; i = i + 1)
+		     mem_r[i] <= mem_r[i - 1];
+		  mem_r[0] <= ext_in;
+		end
+	     else if (cmd_in == DMEM_WRITE)
+		mem_r[addr_in] <= d_in;
+	     else if (cmd_in == DMEM_CLEAR)
+		mem_r <= '0;
+	   end
+      end
+
+
+   always_comb
+      begin:read_mux
+	d_out = mem_r[addr_in];
+      end
+
+   assign sd_out = mem_r[DMEMSIZE-1][DATABITS-1];
+
+
 
 endmodule 
 
