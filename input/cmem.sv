f5e2acc6793ce3279483bb0c63fa54e08e815e65
Meriem Aggoune <maggoune20@kataja2.oulu.fi>
2020-12-24 18:30:27 +0200

    cmem

--- input/cmem.sv
+++ input/cmem.sv
@@ -12,6 +12,32 @@ module cmem
    output logic [DATABITS-1:0] 	      d_out
    );
 
-   
+   logic [CMEMSIZE-1:0][DATABITS-1:0] mem_r;
+   logic [CMEMSIZE*DATABITS-1:0] mem_tmp;
+
+   always_ff@(posedge clk or negedge rst_n)
+      begin:mem_reg
+	if (rst_n == '0)
+	   begin
+	     mem_r <= '0;
+	     mem_tmp = '0;
+	   end
+	else
+	   begin
+	     if (sde_in == '1)
+		begin
+		  mem_tmp = mem_r;
+		  mem_r <= {mem_tmp[$bits(mem_tmp)-2:0], sd_in};
+		end
+	   end
+      end
+
+
+   always_comb
+      begin:read_mux
+	d_out = mem_r[addr_in];
+      end
+
+   assign sd_out = mem_r[CMEMSIZE-1][DATABITS-1];
 endmodule 
 
